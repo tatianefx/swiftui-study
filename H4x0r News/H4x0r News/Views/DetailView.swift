15aa89d1a3485b4306d75232b0f9678c5a6d5781
Tatiane Silva <tatianefx@gmail.com>
2023-09-19 20:39:35 -0300

    Refactor

--- H4x0r News/H4x0r News/Views/DetailView.swift
+++ H4x0r News/H4x0r News/Views/DetailView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct DetailView: View {
     
@@ -20,21 +19,5 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
-    }
-}
-
-struct WebView: UIViewRepresentable {
-
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString, let url = URL(string: safeString) {
-            let request = URLRequest(url: url)
-            uiView.load(request)
-        }
     }
 }
---
+++ H4x0r News/H4x0r News/Views/WebView.swift
@@ -0,0 +1,25 @@
+//
+//  WebView.swift
+//  H4x0r News
+//
+//  Created by Tatiane Silva on 19/09/23.
+//
+
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString, let url = URL(string: safeString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
