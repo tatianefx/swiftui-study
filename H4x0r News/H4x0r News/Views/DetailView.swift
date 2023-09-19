a7633fa64ec4b69eeb633447fb34186da7816c94
Tatiane Silva <tatianefx@gmail.com>
2023-09-19 20:36:49 -0300

    Add detailView

--- H4x0r News/H4x0r News/Views/DetailView.swift
+++ H4x0r News/H4x0r News/Views/DetailView.swift
@@ -6,15 +6,35 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
+    
+    let url: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView(urlString: url)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString, let url = URL(string: safeString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
     }
 }
