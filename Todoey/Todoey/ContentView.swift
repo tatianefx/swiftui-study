269d1870888c4377195ea8a20eafdf78c153b1de
Tatiane Silva <tatianefx@gmail.com>
2023-09-25 18:10:38 -0300

    add checkmark

--- Todoey/Todoey/ContentView.swift
+++ Todoey/Todoey/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var todoeyList = [
+    @State private var todoeyList = [
         Todoey(id: 1, title: "Find Mike"),
         Todoey(id: 2, title: "Buy Eggos"),
         Todoey(id: 3, title: "Destroy Demogorgon")
@@ -17,11 +17,19 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List(todoeyList) { item in
-                Text(item.title)
-                    .onTapGesture {
-                        print("Select \(item.title)")
+            List($todoeyList) { $item in
+                Button {
+                    item.isSelected.toggle()
+                    print("Select \(item.title)")
+                } label: {
+                    HStack {
+                        Text(item.title)
+                        Spacer()
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.blue)
+                            .opacity(item.isSelected ? 1.0 : 0.0)
                     }
+                }
             }
             .navigationTitle("Todoey List")
         }
@@ -37,4 +45,5 @@ struct ContentView_Previews: PreviewProvider {
 struct Todoey: Identifiable {
     let id: Int
     let title: String
+    var isSelected: Bool = false
 }
